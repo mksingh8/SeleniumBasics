9cad6cdc76dd7034588451f13773e5f11737ac99
mksing8 <manish.uit.bu@gmail.com>
2019-01-02 00:10:51 +0530

    updated  the class to add the code to upload the files.

--- selenium/src/seleniumBasics/FileUploadPopUpHandle.java
+++ selenium/src/seleniumBasics/FileUploadPopUpHandle.java
@@ -1,8 +1,25 @@
 package seleniumBasics;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class FileUploadPopUpHandle {
 
 	public static void main(String[] args) {
+		
+
+		System.setProperty("webdriver.gecko.driver", "/home/manish/Downloads/Selenium/Lib/Browser/geckodriver-v0.23.0-linux64/geckodriver");
+		WebDriver driver = new FirefoxDriver();
+		
+
+		//window popup might appear when user clicks on the browse button to find the file to be uploaded. Windows pop
+		//does not supported by Selenium as of now, there is workaround for this issue.
+		
+		driver.navigate().to("https://html.com/input-type-file/");
+		//Browse the file to be uploaded using the send keys command. identify the webelement to upload the file, but
+		//don't use the click command, as window app pop will appear and selenium would not be able to handle it.
+		driver.findElement(By.xpath("//input[@name='fileupload']")).sendKeys("/home/manish/Downloads/MySql/MySql Guide");
 		
 		FileUploadPopUpHandle obj = new FileUploadPopUpHandle();//creating object
 		
