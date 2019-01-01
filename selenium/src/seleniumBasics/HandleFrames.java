b91a513b2dad45f14e6af61cee5b2b1939fbac3c
mksing8 <manish.uit.bu@gmail.com>
2019-01-02 01:25:25 +0530

    commented out static wait code post using dynamic wait

--- selenium/src/seleniumBasics/HandleFrames.java
+++ selenium/src/seleniumBasics/HandleFrames.java
@@ -16,26 +16,28 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 			driver.manage().window().maximize();
 			driver.manage().deleteAllCookies();
 			
-			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 			driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 			
 			driver.navigate().to("https://www.irctc.co.in/nget/train-search");
-			Thread.sleep(2000);
+			//Thread.sleep(2000);
 			
 			//driver.switchTo().frame("chatbotwindow"); //Switch to frame by Name
 			//driver.switchTo().frame(0);//Switch to frame by Index
 			
 			//Switch to frame by WebElement
-			WebElement iFrameElelement = driver.findElement(By.id("chatbotwindow"));//identify the frame by any locators
+			//identify the frame by any locators
+			WebElement iFrameElelement = driver.findElement(By.id("chatbotwindow"));
 			driver.switchTo().frame(iFrameElelement);
 			
 			driver.findElement(By.id("queryInput2")).sendKeys("Train to Hwh from CSMT");
-			Thread.sleep(2000);
+			//Thread.sleep(2000);
 			driver.findElement(By.xpath("//button[contains(text(),'Ask')]")).click();
 			
 			//Switch back to main frame
 			driver.switchTo().defaultContent();
 			
+			//to check if the control is given to the mainframe, enter a value in any text field
 			driver.findElement(By.xpath("//input[@class='ui-inputtext ui-widget ui-state-default "+
 			"ui-corner-all ui-autocomplete-input ng-star-inserted']")).sendKeys("CSMT");
 			Thread.sleep(2000);
