1a2060e95886192a91f431729f8fe861c0aef857
mksing8 <manish.uit.bu@gmail.com>
2019-01-02 00:29:42 +0530

    Added code to maximize the window and delete all cookies before entering the URL

--- selenium/src/seleniumBasics/BrowserFactory.java
+++ selenium/src/seleniumBasics/BrowserFactory.java
@@ -1,5 +1,7 @@
 package seleniumBasics;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -9,7 +11,15 @@ public class BrowserFactory {
 	 
 		System.setProperty("webdriver.gecko.driver", "/home/manish/Downloads/Selenium/Lib/Browser/geckodriver-v0.23.0-linux64/geckodriver");
 		WebDriver driver = new FirefoxDriver();//Launching FF browser
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		
 		driver.get("https://www.google.com");
+		
 		System.out.println(driver.getClass());
 		driver.getCurrentUrl();
 		driver.quit();
