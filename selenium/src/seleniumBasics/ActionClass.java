4166caf6122d953b510102773614502e98ef9396
mksing8 <manish.uit.bu@gmail.com>
2019-01-02 01:26:36 +0530

    corrected formatting and added more comments for clarification

--- selenium/src/seleniumBasics/ActionClass.java
+++ selenium/src/seleniumBasics/ActionClass.java
@@ -16,19 +16,34 @@ import org.openqa.selenium.interactions.Actions;
 			driver.manage().window().maximize();
 			driver.manage().deleteAllCookies();
 			
-			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);//dynamic wait. max time allowed for page to load.
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);//dynamic wait. max wait time, system will 
+			//wait for web-element to appear on page
 			
 			driver.navigate().to("https://www.irctc.co.in/nget/train-search");
-			Thread.sleep(2000);
+			//Thread.sleep(2000);//static wait
 			
 			Actions action = new Actions(driver);
+			
+			//mouse hovering functionality. reach to an element
 			action.moveToElement(driver.findElement(By.xpath("//a[contains(text(),' TRAINS ')]"))).build().perform();
-			Thread.sleep(3000);
+			//Thread.sleep(3000);//static wait is not required if you use dynamic wait
 			driver.findElement(By.xpath("//span[contains(text(),'Book Ticket')]")).click();
 			
-			//action.build().perform();
+			//action.build().perform();//always use this command at the end of action command
 			
+			//performing drag and drop
+			driver.navigate().to("http://jqueryui.com/droppable");
+			
+			//page is having one frame, so switch the control to frame
+			driver.switchTo().frame(0);
+			action.clickAndHold(driver.findElement(By.xpath("//*[@id='draggable']")))
+			.moveToElement(driver.findElement(By.xpath("//*[@id='droppable']"))).release().build().perform();
+			
+			//switch to main frame
+			driver.switchTo().defaultContent();
+			
+			driver.quit();
 
 	}
 
